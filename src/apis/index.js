1b8367691741490184a4a9dc4782e30b60a171b3
jasonpereira96 <jasonpereira074@gmail.com>
2021-05-17 20:29:33 +0530

    adding updates for deployement

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios  from 'axios'
 
 const axiosConfig = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: process.env.VUE_APP_API_URL
 });
 
 axiosConfig.interceptors.request.use(function(config) {
